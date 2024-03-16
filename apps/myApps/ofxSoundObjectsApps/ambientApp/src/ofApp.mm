e47524eb6765eff102bac3e06c01a9b928aa37a0
tctr <thierry.coutier@gmail.com>
2024-03-16 16:39:53 -0700

    clean ambient app (less comments)

--- apps/myApps/ofxSoundObjectsApps/ambientApp/src/ofApp.mm
+++ apps/myApps/ofxSoundObjectsApps/ambientApp/src/ofApp.mm
@@ -22,12 +22,7 @@ void ofApp::setup(){
     mousePoint.y = (ofGetHeight() - rect.height) * 0.5;
 
     //----- Loading sound player begin -------.
-#ifdef USE_LOAD_DIALOG
-    ofFileDialogResult result = ofSystemLoadDialog();
-    if (result.bSuccess) {
-        player.load(result.getPath());
-    }
-#else
+
     ofDirectory dir;
     dir.listDir("sounds");
     for(int i = 0; i < dir.size(); i ++){
@@ -38,57 +33,22 @@ void ofApp::setup(){
                 //set the following to true if you want to stream the audio data from the disk on demand instead of
                 //reading the whole file into memory. Default is false
                 false);
-#endif
+
     //----- Loading sound player end -------.
 
     //----- Sound stream setup begin -------.
-    // the sound stream is in charge of dealing with your computers audio device.
-    // lets print to the console the sound devices that can output sound.
-    ofxSoundUtils::printOutputSoundDevices();
-    
-    auto outDevices = ofxSoundUtils::getOutputSoundDevices();
-    
-    // IMPORTANT!!!
-    // The following line of code is where you set which audio interface to use.
-    // the index is the number printed in the console inside [ ] before the interface name
-    // You can use a different input and output device.
-    
-    int outDeviceIndex = 0;
-    
-    //cout << ofxSoundUtils::getSoundDeviceString(outDevices[outDeviceIndex], false, true) << endl;
-    
-    
     ofSoundStreamSettings soundSettings;
     soundSettings.numInputChannels = 0;
     soundSettings.numOutputChannels = 2;
     soundSettings.sampleRate = player.getSoundFile().getSampleRate();
     soundSettings.bufferSize = 256;
     soundSettings.numBuffers = 1;
-    
     stream.setup(soundSettings);
     
-    
-    // it is important to set up which object is going to deliver the audio data to the sound stream.
-    // thus, we need to set the stream's output. The output object is going to be the last one of the audio signal chain, which is set up further down
     stream.setOutput(output);
-
     //-------Sound stream setup end -------.
     
 
-
-    
-    // ------ waveforms ---------
-    // the waveformDraw class setup receives the rectangle where it is going to be drawn
-    // you can skip this and pass this while drawing if you are changing where this is going to be drawn.
-    // As well, the waveformDraw class inherits from ofRectangle so you can access the functions of the latter.
-    
-    
-    fullFileWaveform.setup( 0, 0, ofGetWidth(), ofGetHeight()/3);
-    wave.setup(0, fullFileWaveform.getMaxY(), ofGetWidth(), ofGetHeight() - fullFileWaveform.getMaxY());
-    // the fullFileWaveform object will have a static waveform for the whole audio file data. This is only created once as it will read the whole sound file data.
-    // For such, we need to get the sound buffer from the sound file in order to get the whole file's data.
-    // calling player.getBuffer(), which actually is a function, will return the players current buffer, the one that is being sent to the sound device, so it will not work for what we are trying to achieve.
-    // the waveformDraw's makeMeshFromBuffer(ofBuffer&) function will create a waveform from the buffer passed
     
     fullFileWaveform.makeMeshFromBuffer( player.getSoundFile().getBuffer());
     
@@ -154,28 +114,6 @@ void ofApp::draw(){
     }
     
     ofPopStyle();
-
-
-/*
-    ofSetColor(ofColor::white);
-
-    fullFileWaveform.draw();
-    
-    ofSetColor(ofColor::red);
-    float playhead = ofMap(player.getPosition(), 0,1, fullFileWaveform.getMinX(),fullFileWaveform.getMaxX());
-    ofDrawLine(playhead, 0, playhead, fullFileWaveform.getMaxY());
-    
-    if(fullFileWaveform.inside(ofGetMouseX(), ofGetMouseY())){
-        ofSetColor(ofColor::cyan);
-        ofDrawLine(ofGetMouseX(), 0, ofGetMouseX(), fullFileWaveform.getMaxY());
-    }
-    ofSetColor(ofColor::white);
-    wave.draw();
-
-    
-    ofSetColor(ofColor::yellow);
-    player.drawDebug(20, 20);
- */
     
 }
 
